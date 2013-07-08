3e6ebc96febc74e6276e169a8ed189c35afea8c7
ich <icanhas@users.sf.net>
2013-07-08 15:34:52 +0100

    etc

--- event.go
+++ event.go
@@ -1,7 +1,15 @@
 package sdl
 
+// #cgo CFLAGS: -D_REENTRANT
+// #cgo LDFLAGS: -lSDL
+// #cgo windows LDFLAGS: -lwinmm -lgdi32 -ldxguid
+//
+// #include <SDL/SDL.h>
+import "C"
+
 import (
 	"time"
+	"unsafe"
 )
 
 var events chan interface{} = make(chan interface{})
@@ -16,48 +24,60 @@ var Events <-chan interface{} = events
 // Polling interval, in milliseconds
 const poll_interval_ms = 10
 
+// Polls for currently pending events
+func (event *Event) poll() bool {
+	GlobalMutex.Lock()
+	ret := C.SDL_PollEvent((*C.SDL_Event)(unsafe.Pointer(event)))
+	if ret != 0 {
+		if (event.Type == VIDEORESIZE) && (currentVideoSurface != nil) {
+			currentVideoSurface.reload()
+		}
+	}
+	GlobalMutex.Unlock()
+	return ret != 0
+}
+
+// pollThread does the polling of events in the thread associated with
+// the global threadlock.
+func (event *Event) pollThread() bool {
+	var status bool
+	thread.Run(func() {
+		status = event.poll()
+	})
+	return status
+}
+
 // Polls SDL events in periodic intervals.
 // This function does not return.
 func pollEvents() {
 	// It is more efficient to create the event-object here once,
 	// rather than multiple times within the loop
 	event := &Event{}
-
 	for {
 		for event.pollThread() {
 			switch event.Type {
 			case QUIT:
-				events <- *(*QuitEvent)(cast(event))
-
+				events <- *(*QuitEvent)(unsafe.Pointer(event))
 			case KEYDOWN, KEYUP:
-				events <- *(*KeyboardEvent)(cast(event))
-
+				events <- *(*KeyboardEvent)(unsafe.Pointer(event))
 			case MOUSEBUTTONDOWN, MOUSEBUTTONUP:
-				events <- *(*MouseButtonEvent)(cast(event))
-
+				events <- *(*MouseButtonEvent)(unsafe.Pointer(event))
 			case MOUSEMOTION:
-				events <- *(*MouseMotionEvent)(cast(event))
-
+				events <- *(*MouseMotionEvent)(unsafe.Pointer(event))
 			case JOYAXISMOTION:
-				events <- *(*JoyAxisEvent)(cast(event))
-
+				events <- *(*JoyAxisEvent)(unsafe.Pointer(event))
 			case JOYBUTTONDOWN, JOYBUTTONUP:
-				events <- *(*JoyButtonEvent)(cast(event))
-
+				events <- *(*JoyButtonEvent)(unsafe.Pointer(event))
 			case JOYHATMOTION:
-				events <- *(*JoyHatEvent)(cast(event))
-
+				events <- *(*JoyHatEvent)(unsafe.Pointer(event))
 			case JOYBALLMOTION:
-				events <- *(*JoyBallEvent)(cast(event))
-
+				events <- *(*JoyBallEvent)(unsafe.Pointer(event))
 			case ACTIVEEVENT:
-				events <- *(*ActiveEvent)(cast(event))
-
+				events <- *(*ActiveEvent)(unsafe.Pointer(event))
 			case VIDEORESIZE:
-				events <- *(*ResizeEvent)(cast(event))
+				events <- *(*ResizeEvent)(unsafe.Pointer(event))
 			}
 		}
-
 		time.Sleep(poll_interval_ms * 1e6)
 	}
 }
